51c9976154a9b0618a992a2b960748b63a678b11
moudev <rigobertomauriciomartinez@gmail.com>
2020-12-05 20:54:28 -0600

    feat: add validator for commands

--- bot.js
+++ bot.js
@@ -1,6 +1,19 @@
 const BOT_PREFIX = '!m-'
+const COMMANDS = ['hi', 'test']
 
 export const validPrefix = (ctx) => {
   const content = ctx.content
   return content.slice(0, BOT_PREFIX.length) === BOT_PREFIX
+}
+
+export const extractCommandWithoutPrefix = (ctx) => {
+  const content = ctx.content
+  const splitContent = content.split(' ')
+  return splitContent[0].slice(BOT_PREFIX.length)
+}
+
+export const existCommand = (ctx) => {
+  const commandToFind = extractCommandWithoutPrefix(ctx)
+  const commandFound = COMMANDS.find(c => c === commandToFind) !== undefined
+  return commandFound
 }
--- index.js
+++ index.js
@@ -1,21 +1,28 @@
+/* eslint no-useless-return: 0 */
+/* eslint no-console: 0 */
 import dotenv from 'dotenv'
 import { Client } from 'discord.js'
-import { validPrefix } from './bot.js'
+import { validPrefix, existCommand } from './bot.js'
 
 dotenv.config()
 const client = new Client()
 
 client.on('ready', () => {
-  // eslint-disable-next-line
   console.log('Bot is working')
 })
 
 client.on('message', ctx => {
   // If the prefix doesn't match do anything
   if (!validPrefix(ctx)) {
-    // eslint-disable-next-line
     return
   }
+
+  if (!existCommand(ctx)) {
+    ctx.channel.send('`That command doesn\'t exist`')
+    return
+  }
+
+  console.log('Valid command')
 })
 
 client.login(process.env.BOT_TOKEN)
